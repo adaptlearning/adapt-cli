238d2441f1d48cb35b91cea92a518d4beeaf27f7
Chris Jones <chris.jones@spongeuk.com>
2014-01-21 17:22:11 +0000

    basic create course command

---
+++ test/specs/create_command_concerns.js
@@ -0,0 +1,45 @@
+var mockery = require('mockery'),
+    sinon = require('sinon'),
+    expect = require('expect.js'),
+    Q = require('q'),
+    Constants = require('../../lib/Constants'),
+    Project = require('../../lib/Project'),
+    Plugin = require('../../lib/Plugin'),
+    rimraf = require('rimraf');
+
+describe('Given I have an create command', function () {
+    describe('when I create a course', function () {
+        var renderer =  {
+                log: sinon.stub()
+            },
+            eventEmitter = {
+                on: function (event, handler) {
+                    if(event === 'close') {
+                        setTimeout(handler, 100);
+                    }
+                    return eventEmitter;
+                }
+            },
+            download = sinon.stub().returns(eventEmitter);
+
+        before(function () {
+            mockery.registerMock('download', download);
+        });
+
+        it('should download the framework', function (done) {
+            mockery.enable({ warnOnUnregistered:false });
+
+            var command = require('../../lib/commands/create');
+            command.create(renderer, 'course', './downloaded-files', 'develop', function () {
+                expect(download.called).to.be(true);
+                done();
+            });
+        });
+
+        after(function () {
+            mockery.deregisterAll();
+            mockery.disable();
+        });
+    });
+    
+});
