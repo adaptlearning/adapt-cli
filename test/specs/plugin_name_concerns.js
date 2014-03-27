2e4ea47bdff48f41779f96668bcb32b2fb0a9851
Chris Jones <chris.jones@spongeuk.com>
2014-03-27 12:19:50 +0000

    plugin version on constructor

--- test/specs/plugin_name_concerns.js
+++ test/specs/plugin_name_concerns.js
@@ -6,24 +6,57 @@ describe('Given I have a name', function () {
             
     describe('when I create a plugin from a package name', function () {
         it('should prefix the name with "adapt"', function () {
-            
             var plugin = new Plugin('package');
             expect(plugin.packageName).to.match(/^adapt-package$/);
         });
+
+        it('should default the version to "0.0.0"', function () {
+            var plugin = new Plugin('package');
+            expect(plugin.version).to.match(/^0.0.0$/);
+        });
+
+        it('should stringify to the bower endpoint', function () {
+            var plugin = new Plugin('package');
+            expect(plugin.toString()).to.match(/^adapt-package$/);
+        });
     });
-    
+
+    describe('when I create a plugin from a specific version package name', function () {
+        it('should parse the correct version', function () {
+            var plugin = new Plugin('package' ,'1.0.0');
+            expect(plugin.packageName).to.match(/^adapt-package$/);
+            expect(plugin.version).to.match(/^1.0.0$/);
+        });
+
+        it('should stringify to the bower endpoint', function () {
+            var plugin = new Plugin('package', '1.0.0');
+            expect(plugin.toString()).to.match(/^adapt-package#1.0.0$/);
+        });
+    });
+
     describe('when I create a contrib plugin from a package name', function () {
         it('should be contrib', function () {
-            
             var plugin = new Plugin('package', true);
             expect(plugin.isContrib).to.be(true);
         });
 
         it('should prefix the name with "adapt-contrib"', function () {
-            
             var plugin = new Plugin('package', true);
             expect(plugin.packageName).to.match(/^adapt-contrib-/);
         });
     });
+
+    describe('when I create a specific version of a contrib plugin from a package name', function () {
+        it('should be contrib', function () {
+            var plugin = new Plugin('package', '1.0.0', true);
+            expect(plugin.isContrib).to.be(true);
+        });
+
+        it('should prefix the name with "adapt-contrib"', function () {
+            var plugin = new Plugin('package', '1.0.0', true);
+            expect(plugin.packageName).to.match(/^adapt-contrib-/);
+        });
+    });
+
 
 });
