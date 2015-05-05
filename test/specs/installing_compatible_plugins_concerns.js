44d5bd5078e2c20ec8805d41d31a77221b65af8b
Chris Jones <chris.jones@spongeuk.com>
2015-05-05 13:14:11 +0100

    version checking

--- test/specs/installing_compatible_plugins_concerns.js
+++ test/specs/installing_compatible_plugins_concerns.js
@@ -1,85 +1,111 @@
 var sinon = require('sinon'),
     expect = require('expect.js'),
     Project = require('../../lib/Project'),
+    RendererHelpers = require('../../lib/RendererHelpers'),
     Q = require('q');
 
 describe('Given that I have Adapt Framework version 2', function () {
     describe('When I install a plugin that is tagged as compatible with Adapt V2 framework version', function () {
         it('should install it', function (done) {
 
-            var getKeywordsSuccessfully = Q.defer(),
-                installSuccessfully = Q.defer();
+            var context = createContext({
+                pluginCompatibility: '~2.0.0',
+                frameworkVersion: '2.0.0'
+            });
 
-            getKeywordsSuccessfully.resolve('adapt-extension');
-            installSuccessfully.resolve(true);
+            var installCommand = require('../../lib/commands/install')(context);
 
-            var stubs = {
-                    bower: {
-                        commands: {
-                            install: sinon.stub()
-                        }
-                    },
-                    renderer: { log: sinon.stub() },
-                    Project: Project,
-                    PackageMeta: {
-                        getKeywords: sinon.stub().returns(getKeywordsSuccessfully.promise)
-                    },
-                    install: sinon.stub().returns(installSuccessfully.promise),
-                };
-
-            sinon.stub(stubs.Project.prototype, 'getFrameworkVersion').returns('2.0.0');
-
-            var installCommand = require('../../lib/commands/install')(stubs);
-
-            installCommand.install(stubs.renderer, 'plugin', function (err) {
-                expect(stubs.install.called).to.be(true);
+            installCommand.install(context.renderer, 'plugin', function () {
+                expect(context.install.called).to.be(true);
                 done();
             });
         });
 
         after(function() {
             Project.prototype.getFrameworkVersion.restore();
+            RendererHelpers.reportCompatibilityWarning.restore();
         });
     });
 });
 
+describe('Given that I have Adapt Framework version 1.1.1 or earlier', function () {
+    describe('When I install a plugin that is tagged as compatible with Adapt V1.1 framework version', function () {
+        it('should install it', function (done) {
+
+            var context = createContext({
+                pluginCompatibility: '1.1.1',
+                frameworkVersion: '1.1.1'
+            });
+
+            var installCommand = require('../../lib/commands/install')(context);
+
+            installCommand.install(context.renderer, 'plugin', function (err) {
+                expect(context.install.called).to.be(true);
+                done();
+            });
+        });
+
+        after(function() {
+            Project.prototype.getFrameworkVersion.restore();
+            RendererHelpers.reportCompatibilityWarning.restore();
+        });
+    });
+});
 
 describe('Given that I have Adapt Framework version 1.1.1 or earlier', function () {
     describe('When I install a plugin that is NOT tagged', function () {
         it('should install it', function (done) {
 
-            var getKeywordsSuccessfully = Q.defer(),
-                installSuccessfully = Q.defer();
+            var context = createContext({
+                renderer: console,
+                pluginCompatibility: '*',
+                frameworkVersion: '1.1.1'
+            });
 
-            getKeywordsSuccessfully.resolve('adapt-extension');
-            installSuccessfully.resolve(true);
+            var installCommand = require('../../lib/commands/install')(context);
 
-            var stubs = {
-                    bower: {
-                        commands: {
-                            install: sinon.stub()
-                        }
-                    },
-                    renderer: { log: sinon.stub() },
-                    Project: Project,
-                    PackageMeta: {
-                        getKeywords: sinon.stub().returns(getKeywordsSuccessfully.promise)
-                    },
-                    install: sinon.stub().returns(installSuccessfully.promise),
-                };
-
-            sinon.stub(stubs.Project.prototype, 'getFrameworkVersion').returns('1.1.1');
-
-            var installCommand = require('../../lib/commands/install')(stubs);
-
-            installCommand.install(stubs.renderer, 'plugin', function (err) {
-                expect(stubs.install.called).to.be(true);
+            installCommand.install(context.renderer, 'plugin', function () {
+                expect(context.install.called).to.be(true);
                 done();
             });
         });
 
         after(function() {
             Project.prototype.getFrameworkVersion.restore();
+            RendererHelpers.reportCompatibilityWarning.restore();
         });
     });
 });
+
+function createContext(values) {
+    var getKeywordsSuccessfully = Q.defer(),
+        getFrameworkCompatibilitySuccessfully  = Q.defer(),
+        installSuccessfully = Q.defer();
+
+    getKeywordsSuccessfully.resolve('adapt-extension');
+    getFrameworkCompatibilitySuccessfully.resolve(values.pluginCompatibility);
+    installSuccessfully.resolve(true);
+
+    var warning = sinon.stub().yields();
+
+    var stubs = {
+        bower: {
+            commands: {
+                install: sinon.stub()
+            }
+        },
+        renderer: values.renderer || { log: sinon.stub() },
+        RendererHelpers: RendererHelpers,
+        Project: Project,
+        PackageMeta: {
+            getKeywords: sinon.stub().returns(getKeywordsSuccessfully.promise),
+            getFrameworkCompatibility: sinon.stub().returns(getFrameworkCompatibilitySuccessfully.promise)
+        },
+        install: sinon.stub().returns(installSuccessfully.promise),
+        warning: warning
+    };
+
+    sinon.stub(stubs.RendererHelpers, 'reportCompatibilityWarning').returns(warning);
+    sinon.stub(stubs.Project.prototype, 'getFrameworkVersion').returns(values.frameworkVersion);
+    return stubs;
+}
